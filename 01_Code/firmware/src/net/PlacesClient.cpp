75c19b76ec96c949d621fbef7877bf3b474d4d26
Daniel Qiu <61307453+yushen-qiu@users.noreply.github.com>
2025-11-02 15:24:39 +1100

    Add Randomness to Places client

--- 01_Code/firmware/src/net/PlacesClient.cpp
+++ 01_Code/firmware/src/net/PlacesClient.cpp
@@ -22,7 +22,8 @@ void fetchNearbyPlace(String includedTypes, int pressCount, double lat, double l
     requestBody += "\"radius\":" + String(pressCount * Config::PLACES_RADIUS_PER_PRESS_M);
     requestBody += "}},";
     requestBody += "\"includedTypes\":" + includedTypesJson + ",";
-    requestBody += "\"maxResultCount\":1";
+    requestBody += "\"rankPreference\": \"DISTANCE\","
+    requestBody += "\"maxResultCount\":3";
     requestBody += "}";
 
     Serial.println(requestBody);
@@ -36,13 +37,14 @@ void fetchNearbyPlace(String includedTypes, int pressCount, double lat, double l
     int httpCode = http.POST(requestBody);
 
     if (httpCode > 0) {
-        String               payload = http.getString();
+        String payload = http.getString();
         DynamicJsonDocument  doc(4096);
         DeserializationError err = deserializeJson(doc, payload);
 
         if (!err && !doc["places"][0]["location"].isNull()) {
-            destinationLatitude  = doc["places"][0]["location"]["latitude"];
-            destinationLongitude = doc["places"][0]["location"]["longitude"];
+            int randomIndex = random(0, 3);
+			destinationLatitude = doc["places"][randomIndex]["location"]["latitude"];
+			destinationLongitude = doc["places"][randomIndex]["location"]["longitude"];
             Serial.printf("Nearby place: %.6f, %.6f\n", destinationLatitude, destinationLongitude);
         } else {
             Serial.println("Error parsing Places API response");
